5ad8cae1f590e692a1283cf2c4da6ad361ff6067
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-08-31 19:32:04 +0530

    Map in Js

---
+++ Javascript/05_iterations/07_map.js
@@ -0,0 +1,22 @@
+// l-30
+// +++++++++++++++++++++++    Map   +++++++++++++++++++
+//map::-  callbackfn 
+//Most of ppl feel it is better than forEach
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map( (num) => num + 10)   //Each number as num
+// console.log(newNums); //It automatically returns value
+
+
+// const newNum = myNumbers.map( (num) => { return num + 10})   //with scope
+// console.log(newNum); 
+
+// ++++++++++        Chaining       ++++++++++++++++++++++++
+ /**
+ u can use 2 3 method directly   (myNum.map().map().filter())
+ */
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// const newNums
