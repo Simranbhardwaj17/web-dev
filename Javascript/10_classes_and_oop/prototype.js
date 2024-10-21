6836e09437d3541d15e80f1029e05499c84e6ba5
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-10-21 21:00:34 +0530

    Prototype overview

---
+++ Javascript/10_classes_and_oop/prototype.js
@@ -0,0 +1,29 @@
+//L-43
+//D-21/10/24
+
+//Js behavior- prototype behavior(for inst)
+//Js Mechanism -Why, how Js func
+
+//In Browser console
+
+const newH = ['sb', 'cc']
+newH     // (after click on it u will see prototype)
+
+// Js behavior: prototypal behavior(Js go above on above the layer(parent access GP) to find value until get null)
+// By linking to this behav this keyword works. In arrow func this keyword not coz of proto there
+// coz of prototype get access to new keyword, class, this keyw, prototypal inheritance
+
+// Its behavior if not know ask someone(haar nhi manna cont. to find(layer by layer))
+
+
+
+
+
+
+
+
+
+
+
+
+
