230e4557857cc5714e0c9f9b617bd7baafd12546
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-11-16 00:48:27 +0530

    Create own function in basic react app

---
+++ React/01basicreact/src/Chai.js
@@ -0,0 +1,17 @@
+//13-11-2024
+function chai() {
+    return(
+        <h2> Basic react app </h2>
+    )
+}
+
+export default chai
+
+
+//here, chai is a component (here, u export func, that is component)
+
+//it is important to capitalise the function name
+
+//If component return html then name it as .jsx
+
+
