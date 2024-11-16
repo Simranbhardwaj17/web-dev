53c2f7e7eacaf85353c83047be2cf034e6d5192f
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-11-17 01:45:14 +0530

    Learning to render elements

--- React/01vitereact/src/main.jsx
+++ React/01vitereact/src/main.jsx
@@ -3,11 +3,42 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp() {
+    return(
+        <div>
+            <h1>Custom App | Simran </h1>
+        </div>
+    )
+}
+
+//React convert this above HTML syntax into below obj syn(to form tree)
+
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+
+const anotherElement = (
+    <a href= "https://google.com" target = "_blank"></a>
+)
+
+//Its react render
 createRoot(document.getElementById('root')).
 render(
   
-    <App />
+    // <App />
+    //<MyApp />    //React use bundler which convert/parse these syntax into preferrable syntax(upgrade syn), work BTS 
+    // MyApp
+    // ReactElement()    not working
+    anotherElement
   
 )
 
 //react create root & in that we pass a doc & render the app & app is a simple func which contain jsx
+
+
