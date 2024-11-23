196ec4e8b72acdc8fa41b2da6e8d689cebd7de6c
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-11-23 20:34:20 +0530

    Main file

--- React/01vitereact/src/main.jsx
+++ React/01vitereact/src/main.jsx
@@ -13,6 +13,7 @@ function MyApp() {
 
 //React convert this above HTML syntax into below obj syn(to form tree)
 
+//Not working coz of syntax defined by us (not a/c to react)
 // const ReactElement = {
 //     type: 'a',
 //     props: {
@@ -22,14 +23,20 @@ function MyApp() {
 //     children: 'Click me to visit google'
 // }
 
+const anotherUser = "Coder S"
 
-// This time create a/c to react(not us)  use React
+// This time create a/c to react(not us)  use React createElement() format. So, its working
 const reactElement = React.createElement(
-    'a',
+    'a',      //type
     {href: 'https;//google.com', target: '_blank'},
-    'Click me to visit google'
+    'Click me to visit google',
+    //to inject anotherUser, after tree creation var injection occur
+    anotherUser      //final exp is the evluated expression that is injected at last
 )
+//u can't use here(inside reactElement) if else coz of js syntax(in obj it is not allowed) & it is evluated expression
 
+
+//It is  working coz of only HTML present
 const anotherElement = (
     <a href= "https://google.com" target = "_blank">Visit google</a>
 )
@@ -42,8 +49,8 @@ render(
     //<MyApp />    //React use bundler which convert/parse these syntax into preferrable syntax(upgrade syn), work BTS 
     // MyApp
     // ReactElement()    not working
-    anotherElement
-    // reactElement
+    // anotherElement
+    reactElement
 )
 
 //react create root & in that we pass a doc & render the app & app is a simple func which contain jsx
