1dde36c31498afa48f6f7282eef6b2aac0ab9931
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-12-08 19:25:01 +0530

    Hooks use

--- React/02counter/src/App.jsx
+++ React/02counter/src/App.jsx
@@ -5,10 +5,14 @@ import './App.css'
 
 function App() {
   
-  let counter= 14
+  const [counter, setCounter] = useState(15)          //how to use hooks(not use const)
+  // let counter = 15
 
   const addValue = () => {
-    console.log("Clicked", Math.random());
+    console.log("Clicked", counter);
+    // counter = counter + 1;
+    // setCounter(counter)       or
+    setCounter(counter + 1)             //help to sink 
   }
 
   return (
@@ -16,9 +20,10 @@ function App() {
       <h1>Counter Project</h1>
       <h2>Counter Value: {counter}</h2>
 
-      <button onClick={addValue}>Add Value</button>  
+      <button onClick={addValue}>Add Value {counter}</button>  
       <br></br>
-      <button>Removed Value</button>
+      <button>Removed Value {counter}</button>
+      <p> Footer: {counter}</p>
     </>
   )
 }
@@ -27,3 +32,21 @@ export default App
 
 
 //Method passed not want to directly execute. so, remove " () " from addValue
+
+//13:41  :- React hooks(doc)
+
+// React hook is needed to update UI
+
+//Every hook have special task assigned. For big library, need to make modular library only 1 task assigned to 1 function
+// coz of updation & debug of code
+
+//React control UI updation
+
+//No need to import react still above method go & tree parsing is done coz of babel _jsx
+
+//How to use hooks:-
+//useState is used for change state(not just value change propagate it in UI/DOM) default value 
+//to update var use method at 2nd place named as "setCounter" or "setIsLoggedin"
+//no need to use 
+// let counter = 14     to decl         &
+//counter = counter + 1   ::to update
