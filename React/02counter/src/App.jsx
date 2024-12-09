ed64a4697bd2f4c3f9d83564d393ff5052769aa1
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-12-09 16:03:11 +0530

    Hooks

--- React/02counter/src/App.jsx
+++ React/02counter/src/App.jsx
@@ -3,32 +3,42 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+//How changes propagate in react 
+//hooks:- display on UI
+
+// function App() {
   
-  const [counter, setCounter] = useState(15)          //how to use hooks(not use const)
-  // let counter = 15
+//   let [counter, setCounter] = useState(15)          //how to use hooks(not use const)    {setCounter can be named as anything}
+//   // let counter = 15
 
-  const addValue = () => {
-    console.log("Clicked", counter);
-    // counter = counter + 1;
-    // setCounter(counter)       or
-    setCounter(counter + 1)             //help to sink 
-  }
+//   const addValue = () => {
+//     // counter = counter + 1;
+//     // setCounter(counter)       or
+//     setCounter(counter + 1)             //help to sink state & UI
+//     // console.log("Clicked", counter);
+//   }
 
-  return (
-    <>
-      <h1>Counter Project</h1>
-      <h2>Counter Value: {counter}</h2>
+//   const removeValue = () => {
+//     setCounter(counter - 1)
+//   }
 
-      <button onClick={addValue}>Add Value {counter}</button>  
-      <br></br>
-      <button>Removed Value {counter}</button>
-      <p> Footer: {counter}</p>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <h1>Counter Project</h1>
+//       <h2>Counter Value: {counter}</h2>
 
-export default App
+//       <button 
+//       onClick={addValue}>Add Value {counter}</button>  
+//       <br></br>
+//       <button
+//       onClick={removeValue}
+//       >Remove Value {counter}</button>
+//       <p> Footer: {counter}</p>
+//     </>
+//   )
+// }
+
+// export default App
 
 
 //Method passed not want to directly execute. so, remove " () " from addValue
@@ -47,6 +57,53 @@ export default App
 //How to use hooks:-
 //useState is used for change state(not just value change propagate it in UI/DOM) default value 
 //to update var use method at 2nd place named as "setCounter" or "setIsLoggedin"
-//no need to use 
+//no need to use-- 
 // let counter = 14     to decl         &
 //counter = counter + 1   ::to update
+
+//State get updated everywhere no need to take references
+
+
+//++++++++++++++++++++++++++++
+//Assignment 
+//Q:- Use condition for addValue(till 20) & removeValue(till 0(no -ve values))
+
+function App() {
+  
+  let [counter, setCounter] = useState(15)          //how to use hooks(not use const)    {setCounter can be named as anything}
+  // let counter = 15
+
+  const addValue = () => {
+    // counter = counter + 1;
+    // setCounter(counter)       or
+    // setCounter(counter + 1)             //help to sink state & UI
+    // console.log("Clicked", counter);
+    if(counter <= 19 ) {
+      setCounter(counter + 1);
+    }
+  }
+
+  const removeValue = () => {
+    // setCounter(counter - 1)
+    if(counter >= 1) {
+      setCounter(counter - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button 
+      onClick={addValue}>Add Value {counter}</button>  
+      <br></br>
+      <button
+      onClick={removeValue}
+      >Remove Value {counter}</button>
+      <p> Footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
