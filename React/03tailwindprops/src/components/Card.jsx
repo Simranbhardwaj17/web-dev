571529b6e05b58ee7eb9611da130cd1130755896
Simranbhardwaj17 <simranbhardwaj1706@gmail.com>
2024-12-13 18:59:37 +0530

    Tailwindprops

---
+++ React/03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Card() {
+    return (
+        <div>Card</div>
+    )
+}
+
+export default Card
+
+//component behave as html
+
+//for shortcut replace props by obj (username) directly (destructure)
+
+//to handle/display all prop pass those too & inject js var
+//u can also use/pass def value so that, in case val not passed then def value work
+
+//to access obj use .
+
+//to pass different value in same card pass your own def value
+
+//u can pass any value in components like obj/ array & also can access
+
+//dev UI library, configure tailwind
