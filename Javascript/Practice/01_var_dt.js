d5c5e6873534880928998684cdc9747efb76a0fd
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-12-04 22:57:19 +0530

    Solve

---
+++ Javascript/Practice/01_var_dt.js
@@ -0,0 +1,26 @@
+// Q. Create a const object called "product" to store information shown in the picture of given product .
+// code:
+const product = {
+  pName: "Parker Jotter Standard CT Ball Pen",
+  ink: "Black",
+  star: 4,
+  rating: 4567,
+  deal: "Deal of the day",
+  price: 234,
+  isDeal: true,
+}
+console.log(product);
+
+// Q. Create a const object called "profile" to store information shown in the picture of twitter.
+// code:
+const profile = {
+  username: "simu",
+  isFollow: true,
+  following: 0,
+  followers: 45,
+  about: "Engineering Student",
+}
+
+console.log(profile);
+console.log(typeof profile["username"]);
+console.log(profile.followers);
