44e7ece197627f872715682e6590818d6017fa0e
Simran Bhardwaj <simranbhardwaj1706@gmail.com>
2025-12-05 22:28:14 +0530

    Operators

---
+++ Javascript/Practice/02_op_conditionals.js
@@ -0,0 +1,42 @@
+//Arithmetic operators
+
+let a = 5;
+let b = 2;
+
+console.log("a =", a, "," ,"b=", b);
+
+console.log("a + b =", a+b);
+console.log("a - b =", a-b);
+console.log("a * b =", a*b);
+console.log("a / b =", a/b);  //2.5
+console.log("a % b =", a%b);  //1
+console.log("a ** b =", a**b); //25
+
+
+//Unary operator
+console.log("a++ =", a++);
+console.log("a =", a);
+console.log("++a =", ++a);
+
+console.log("--a =", --a);
+console.log("a-- =", a--);
+console.log("a =", a);
+
+
+//Assign op
+// += , -= , = , %= , **=
+console.log("c = ", c=a);
+console.log("a=a+3 =", a += 3);
+
+a **= 4;
+console.log("a = ", a);
+
+//comparison 
+// Equal to ==
+// Not equal to !=
+// Equal to & type ===
+// Not Equal to & type  !==
+//  >, >=, < , <=
+
+console.log("a == b", a==b);
+console.log("a != b", a!=b);
